bba5fd7944e12fc56e26bc6579b7990b97ffa884
Joyrudra <joyrudrabws07@gmail.com>
2025-05-14 20:03:33 +0530

    chore: mograted the repo to vite, react 19

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig, transformWithEsbuild } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [
+    {
+      name: "treat-js-files-as-jsx",
+      async transform(code, id) {
+        if (!id.match(/src\/.*\.js$/)) return null;
+
+        // Use the exposed transform from vite, instead of directly
+        // transforming with esbuild
+        return transformWithEsbuild(code, id, {
+          loader: "jsx",
+          jsx: "automatic",
+        });
+      },
+    },
+    tailwindcss(),
+    react(),
+  ],
+
+  optimizeDeps: {
+    force: true,
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
+      },
+    },
+  },
+});
